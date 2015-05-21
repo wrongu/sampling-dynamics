c21ce7ee65133bd4b056c8fddfcf72c5f0d01982
wrongu <richard.d.lange.13@dartmouth.edu>
2015-05-20 20:07:12 -0400

    load_or_run prints what it is doing

--- util.py
+++ util.py
@@ -1,14 +1,16 @@
 import numpy as np
 import os
 
-def load_or_run(filename, function, subdir='saved_data', force_recompute=False):
+def load_or_run(filename, function, subdir='saved_data', force_recompute=False, verbose=True):
 	if filename[-4:] != '.npy':
 		filename += '.npy'
 	filename = os.path.join(subdir, filename)
 	if force_recompute or not os.path.isfile(filename):
+		if verbose: print "%s not found; computing from scratch" % filename
 		data = function()
 		np.save(filename, data)
 	else:
+		if verbose: print "loading data from %s" % filename
 		data = np.load(filename)
 	return data
 
